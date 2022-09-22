5ad8f001a82d6d98bfae78580a8a9b930fbed55b
durgaprasadpara <112737459+durgaprasadpara@users.noreply.github.com>
2022-09-22 10:04:43 +0530

    Add files via upload

---
+++ fiboo.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class fiboo
+{
+    public static void main(String[] args) 
+    {
+      try
+        {
+        int n, a = 0, b = 0, c = 1;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter value of n:");
+        n = s.nextInt();
+     if(n<=0)
+     {
+        System.out.println("Invalid");
+     }
+    else
+    {
+        System.out.print("Fibonacci Series:");
+        for(int i = 1; i <= n; i++)
+        {
+            a = b;
+            b = c;
+            c = a + b;
+            System.out.print(a+" ");
+        }
+    }
+}
+ catch(Exception e)
+     {
+   System.out.println("Invalid due to floating point");
+       }
+    }
+}
